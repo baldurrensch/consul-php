fc8ef4e568e460a08b65fbb2b9e73a5651b64cb8
Baldur Rensch <brensch@gmail.com>
2014-05-14 00:28:13 -0700

    Add more tests

---
+++ tests/Model/ServiceListTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace BR\Consul\Test\Model;
+
+use BR\Consul\Model\ServiceList;
+
+class ServiceListTest extends \PHPUnit_Framework_TestCase
+{
+    public function offsetExists()
+    {
+        $serviceList = new ServiceList([0 => true, 1 => true, 2 => true]);
+
+        $this->assertTrue($serviceList->offsetExists(1));
+        $this->assertFalse($serviceList->offsetExists(4));
+    }
+
+    public function offsetSet()
+    {
+        $serviceList = new ServiceList([0 => true, 1 => true, 2 => true]);
+
+        $serviceList->offsetSet(0, 'new value');
+        $this->assertEquals('new value', $serviceList[0]);
+
+        $serviceList->offsetSet(4, 'new value');
+        $this->assertEquals('new value', $serviceList[4]);
+    }
+
+    public function testOffsetUnset()
+    {
+        $serviceList = new ServiceList([0 => true, 1 => true, 2 => true]);
+
+        $serviceList->offsetUnset(0);
+        $this->assertArrayNotHasKey(0, $serviceList);
+    }
+} 
---
+++ tests/Model/ServiceTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BR\Consul\Test\Model;
+
+use BR\Consul\Model\Service;
+
+class ServiceTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAddTag()
+    {
+        $service = new Service();
+
+        $service->addTag('abc');
+        $this->assertEquals(['abc'], $service->getTags());
+
+        $service->addTag('def');
+        $this->assertEquals(['abc', 'def'], $service->getTags());
+    }
+} 
